28d3747995e769d7221fbdda5ea9d9c280d1761c
ayako56 <ayako56@users.noreply.github.com>
2016-06-10 10:19:04 +0900

    add code

---
+++ sample/sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by 石原理子 on 2016/03/10.
+//  Copyright © 2016年 石原理子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
